ff345ba91ad62fa177534e1329013ed731506fac
Charles Dawes <cdawes@divisionsinc.com>
2021-10-18 15:39:43 -0400

    init

---
+++ DaprCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Client;
+
+namespace DaprCounter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string storeName = "statestore";
+            const string key = "counter";
+
+            var daprClient = new DaprClientBuilder().Build();
+            var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+            while (true)
+            {
+                Console.WriteLine($"Counter = {counter++}");
+                await daprClient.SaveStateAsync(storeName, key, counter);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
